7af444f6b86b443246d5e5085291be0987536bb4
Vilma Evborn <vilma.evborn@elev.ga.ntig.se>
2024-09-26 11:09:22 +0200

    commit

--- Arsenal/Hat.cs
+++ Arsenal/Hat.cs
@@ -1,4 +1,17 @@
+using System.Text.Json.Serialization;
+
 public class Hat
 {
+    [JsonInclude]
+    public string Name;
+
+    [JsonInclude]
+    public int Swag;
+
+    [JsonInclude]
+    public int Armor;
+    
+    [JsonInclude]
+    public string Color;
 
 }
--- Arsenal/Program.cs
+++ Arsenal/Program.cs
@@ -1,1 +1,28 @@
-﻿
+﻿global using System.Text.Json;
+using System.Text.Json.Serialization;    //hela världen använder json - all kommunikation (utom html, css) - serialiserare = göra om objekt till text och tvärtom
+
+string textHat = File.ReadAllText("hats.json");
+
+List<Hat> hats = JsonSerializer.Deserialize<List<Hat>>(textHat);
+
+Console.WriteLine(hats[1].Name);
+
+Console.ReadLine();
+
+
+
+
+
+
+// Hat myFirstHat = new Hat()
+// {
+//     Name = "Fedora",
+//     Swag = 10,
+//     Armor = 3,
+//     Color = "Black"
+// };
+
+// string jsonHat = JsonSerializer.Serialize(myFirstHat);
+// Console.WriteLine(jsonHat);
+
+// File.WriteAllText("fedora.json", jsonHat);
